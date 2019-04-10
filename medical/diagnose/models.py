9add1588cbf211722f79c04ee537c32519c59076
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-10 19:09:35 +0200

    Modified `medical.diagnose.models.Diagnose` class to contain the date and time.

--- medical/diagnose/models.py
+++ medical/diagnose/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from profiles.models import Doctor, Patient
+from datetime import datetime
 
 class Diagnose(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     content = models.TextField()
+    date_time = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
-        return self.content[:20] + '...'
+        return self.content[: 20] + ' ...'
